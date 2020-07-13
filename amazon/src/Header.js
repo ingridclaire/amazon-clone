443d66c9b45046d006c9fa27bdf2e539626b50b0
Ingrid Carlson <ingridclaire@gmail.com>
2020-07-13 11:53:53 -0700

    inititated app, added routes and header

---
+++ amazon/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <nav className="header">
+
+    <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+
+    </nav>
+  )
+}
+
+export default Header;
