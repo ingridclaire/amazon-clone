4dbb6bd1c39e2043115a47149e4603bcf8a28772
Ingrid Carlson <ingridclaire@gmail.com>
2020-07-14 13:57:47 -0700

    finished sign in/sign out details, initialized with firebase

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header.js';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
+import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function App() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        });
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  console.log(user);
+
   return (
     <Router>
       <div className="app">
